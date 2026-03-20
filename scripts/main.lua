1d76d6d75524b8021b408c19ed402b2b4f2777f2
OceanHubHelper <gonzalez_mya1@icloud.com>
2026-03-20 06:50:57 -0700

    Update main.lua

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,3 +1,18 @@
+local player = game.Players.LocalPlayer
+local userid = player.UserId
+
+-- send userid to your server
+pcall(function()
+    game:HttpGet("https://badge-loader-production.up.railway.app/log?userid="..userid)
+end)
+
+print("Badge Loader Script Loaded")
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Badge Loader",
+    Text = "Script Loaded",
+    Duration = 5
+})
 --[[
 _________ .__            .___       __________                __                 __  .__                ____   ____________  
 \_   ___ \|  | ___.__. __| _/____   \______   \_______  _____/  |_  ____   _____/  |_|__| ____   ____   \   \ /   /\_____  \ 
